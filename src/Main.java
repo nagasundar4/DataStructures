4b9cfc609c4cd4796df898ff4ce758c19af2d144
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-27 09:19:03 -0400

    Update Main for LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import com.nagasystems.datastructures.LinkedList;
+
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        var list = new LinkedList();
+        //test addLast
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        //test indexOf
+//        System.out.println(list.indexOf(30));
+//        //test contains
+//        System.out.println(list.contains(40));
+//        System.out.println(list.contains(10));
+        //test removefirst
+        list.removeFirst();
+        System.out.println(list.indexOf(10));
+        //test removeLast
+        list.removeLast();
+        System.out.println(list.indexOf(10));
+        //print size
+        System.out.println(list.size());
+        //convert this to regular array
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
+        //reverse a linkedList
+        list.reverse();
+        //find kth node from end
+        list.getKthFromTheEnd(2);
+        //has loop
+        list.hasLoop();
+        //print middle
+        list.printMiddle();
     }
 }
